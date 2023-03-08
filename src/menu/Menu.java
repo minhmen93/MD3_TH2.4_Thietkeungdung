b118477e05d6b5e6c39bfdf7154de634ec992b93
unknown <minhmen93@gmail.com>
2023-03-08 15:04:58 +0700

    thiet ke ung dung

---
+++ src/menu/Menu.java
@@ -0,0 +1,52 @@
+package menu;
+
+import java.util.Scanner;
+
+public class Menu {
+    public static void main(String[] args) {
+        int choice = -1;
+        Scanner input = new Scanner(System.in);
+        while (choice != 0){
+            System.out.println("Menu");
+            System.out.println("1. Vẽ hình tam giác");
+            System.out.println("2. Vẽ hình vuông");
+            System.out.println("3. Vẽ hình chữ nhật");
+            System.out.println("0. exit");
+            System.out.println(" In ra hình ");
+            choice = input.nextInt();
+            switch (choice){
+                case 1 :
+                    System.out.println("Vẽ hình tam giác");
+                    System.out.println("*");
+                    System.out.println("**");
+                    System.out.println("***");
+                    System.out.println("****");
+                    System.out.println("*****");
+                    System.out.println("******");
+                    System.out.println("*******");
+                    System.out.println("********");
+                    break;
+                case 2 :
+                    System.out.println("Vẽ hình vuông");
+                    System.out.println("***********");
+                    System.out.println("***********");
+                    System.out.println("***********");
+                    System.out.println("***********");
+                    System.out.println("***********");
+                    break;
+                case 3:
+                    System.out.println("Vẽ hình chữ nhật");
+                    System.out.println("******************");
+                    System.out.println("******************");
+                    System.out.println("******************");
+                    System.out.println("******************");
+                    break;
+                case 0:
+                    System.exit(0);
+                default:
+                    System.out.println("No Choice !");
+
+            }
+        }
+    }
+}
